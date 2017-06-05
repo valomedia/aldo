4ea7f540f7153f8cf0bb07378a6cb9afa34041dd
Jean-Pierre Höhmann <jp@hhmn.de>
2017-06-05 03:48:06 +0200

    Add page settings.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
 
+export class Page {
+    id: number;
+    name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}!</h1>`,
+  template: `
+    <h1>{{name}}<br><small>{{page.name}}</small></h1>
+    <div><label>id: </label>{{page.id}}</div>
+    <div>
+        <label>name:</label>
+        <input [(ngModel)]='page.name' placeholder="Seitenname">
+    </div>
+    `
 })
-
-export class AppComponent  { name = 'Aldo'; }
+export class AppComponent  {
+    name = 'Aldo';
+    page: Page = {id: 1, name: "Meine Seite"};
+}
 
