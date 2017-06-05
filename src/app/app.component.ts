5b4ba9e39cc1afcc18a2bbe82c2844c041398b0c
Jean-Pierre Höhmann <jp@hhmn.de>
2017-06-05 18:31:06 +0200

    Add page picker.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
 
+const PAGES: Page[] = [
+    {id: 1, name: "Meine Seite"},
+    {id: 2, name: "Deine Seite"},
+    {id: 3, name: "Seine Seite"},
+    {id: 4, name: "Ihre Seite"},
+    {id: 5, name: "Andere Seite"},
+    {id: 6, name: "Unsere Seite"},
+    {id: 7, name: "Eure Seite"},
+    {id: 8, name: "Noch eine Seite"}
+];
+
 export class Page {
     id: number;
     name: string;
@@ -8,16 +19,32 @@ export class Page {
 @Component({
   selector: 'app',
   template: `
-    <h1>{{name}}<br><small>{{page.name}}</small></h1>
-    <div><label>id: </label>{{page.id}}</div>
+    <h1>{{name}}</h1>
+    <div *ngIf='page'>
+        <h2>{{page.name}} – Detailansicht</h2>
+        <div><label>id: </label>{{page.id}}</div>
+        <div>
+            <label>name:</label>
+            <input [(ngModel)]='page.name' placeholder="Seitenname">
+        </div>
+    </div>
     <div>
-        <label>name:</label>
-        <input [(ngModel)]='page.name' placeholder="Seitenname">
+        <h2>Seiten</h2>
+        <ul class='pages'>
+            <li
+                    *ngFor='let e of pages'
+                    (click)='select(e)'
+                    [class.selected]='e === this.page'>
+                <span class='badge'>{{e.id}}</span>{{e.name}}
+            </li>
+        </ul>
     </div>
     `
 })
 export class AppComponent  {
     name = 'Aldo';
-    page: Page = {id: 1, name: "Meine Seite"};
+    pages = PAGES;
+    page: Page;
+    select = (page: Page): Page => this.page = page;
 }
 
