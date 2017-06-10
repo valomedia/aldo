d9b1de647c40e3cfb7a53fa415b3999730cf08fb
Jean-Pierre Höhmann <jp@hhmn.de>
2017-06-10 18:04:05 +0200

    Add basic error handling.

--- src/script.js
+++ src/script.js
@@ -19,9 +19,20 @@ window.fbAsyncInit = function() {
     FB.AppEvents.logPageView();
 
     // Ask the user to log in to Facebook.
-    FB.login(function() {
-        // Launch Aldo.
-        System.import('main.js').catch(function(err){ console.error(err); });
+    FB.login(function(res) {
+        switch (res.status) {
+            case 'connected':
+                // User is logged in and authorized the app, launch Aldo.
+                System.import('main.js').catch(function(err){ console.error(err); });
+                break;
+            case 'not_authorized':
+                // User is logged in, but has not given permissions, error out.
+                alert('Auth failed');
+                break;
+           default:
+               // User is not logged in, error out.
+               alert('Not logged in');
+        }
     },
     {
         scope: [
