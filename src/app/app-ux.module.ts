360d957c3a03a75a4dfa2b29a01611d309ae2c12
Jean-Pierre Höhmann <jp@hhmn.de>
2017-06-28 19:07:26 +0200

    Add AppUxModule.

---
+++ src/app/app-ux.module.ts
@@ -0,0 +1,12 @@
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+
+/*
+ * The root module for the material modules.
+ */
+
+@NgModule({
+    imports: [MdButtonModule, MdCheckboxModule],
+    exports: [MdButtonModule, MdCheckboxModule]
+})
+export class AppUxModule {}
+
