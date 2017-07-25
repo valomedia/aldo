ab3d1285a1510fc68a0d1e44bac1e3f4efbdd526
Jean-Pierre Höhmann <jp@hhmn.de>
2017-07-25 21:32:42 +0200

    Add Post.

---
+++ src/app/post.ts
@@ -0,0 +1,44 @@
+
+/*
+ * Classes related to Facebook posts.
+ */
+
+/*
+ * A Facebook post as returned by the Facebook API.
+ */
+export interface PostType {
+    id: number;
+    message: string;
+    story: string;
+    created_time: string;
+}
+
+/*
+ * A Facebook post as used internally.
+ */
+export class Post {
+    constructor(kwargs: PostType) { Object.assign(this, kwargs); }
+
+    getText() {
+        return this.message || this.story;
+    }
+
+    getCreatedTime() {
+        return new Date(this.created_time);
+    }
+}
+export interface Post extends PostType {}
+
+/*
+ * The simplest valid profile.
+ *
+ * This exists, so the Users and Pages can use it to build their EMPTY 
+ * constants.
+ */
+export const EMPTY_POST = new Post({
+    id: 0,
+    message: '',
+    story: '',
+    created_time: ''
+});
+
