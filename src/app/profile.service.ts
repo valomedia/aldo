0a120b7e8d374ead80159cbc853e592e0dc9dd6d
Jean-Pierre Höhmann <jp@hhmn.de>
2017-10-01 23:07:27 +0200

    Fix a cacheing bug.

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/publishReplay';
 
 import {Profile, DUMMY_PROFILE_TYPE} from './profile';
 import {FbService, HttpMethod} from './fb.service';
@@ -34,12 +35,19 @@ export class ProfileService {
      * Get a Profile by its ID.
      */
     profile(id: string): Observable<Profile> {
-        return this._profile(id).mergeMap(profile => {
-            switch (profile.metadata.type) {
-                case 'page': return this.pageService.page(id);
-                default: return Observable.of(profile);
-            }
-        });
+        // Need to redo the cacheing here, as we would otherwise mergeMap the 
+        // cached instance of Profile the a new instance of a subtype of Profile 
+        // every time this Observable is subscribed to.
+        return this._profile(id)
+            .mergeMap(profile => {
+                switch (profile.metadata.type) {
+                    case 'page': return this.pageService.page(id);
+                    default: return Observable.of(profile);
+                }
+            })
+            .publishReplay(1)
+            .refCount()
+            .first();
     }
 }
 
