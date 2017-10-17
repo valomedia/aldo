a4baf7bd99203dc270ec57d4fec0e3e35b48dc7a
Jean-Pierre Höhmann <jp@hhmn.de>
2017-10-17 16:22:50 +0200

    Add Settings.

---
+++ src/app/settings.ts
@@ -0,0 +1,15 @@
+
+/*
+ * The Settings the user can set in the app.
+ */
+
+export class Settings {
+
+    /*
+     * Settings, that change the optics of the application.
+     */
+    ui = {
+        dark: false
+    }
+}
+
